6bf7d03803f44cb590f33550d9d47fd4e1989198
paul-ruban <paulruban@qq.com>
2020-06-27 15:38:12 -0400

    improved GUI, dealt with input validation

--- main.py
+++ main.py
@@ -6,52 +6,54 @@ from sorting_algorithms.heap_sort import heap_sort
 from sorting_algorithms.quick_sort import quick_sort
 from matplotlib.animation import FuncAnimation
 from matplotlib import pyplot as plt
-from tkinter import Tk, Button, Radiobutton, Entry, StringVar
+from tkinter import Tk, Frame, Button, Radiobutton, Scale, Label, StringVar, IntVar, BOTH, BOTTOM, HORIZONTAL, font
 from random import shuffle
 
-def callback(sv, btn):
-    try:
-        n = int(sv.get())
-    except ValueError:
-        btn['state'] = 'disabled'
-        return
-    btn['state'] = 'normal' if n > 0 and n < 1000 else 'disabled'
+"""dictionary containing sorting algorithms, their complexity 
+and the generators that yield values along the sorting process"""
+sort_dict = {
+        'selection_sort' : { 'title' : r'Selection Sort ($O(n^2)$)', 'generator' : lambda data: selection_sort(data)},
+        'insertion_sort' : { 'title' : r'Insertion Sort ($O(n^2)$)', 'generator' : lambda data: insertion_sort(data)},
+        'bubble_sort' : { 'title' : r'Bubble Sort ($O(n^2)$)', 'generator' : lambda data:bubble_sort(data)},
+        'merge_sort' : { 'title' : r'Merge Sort ($O(n log(n))$)', 'generator' : lambda data: merge_sort(data, 0, len(data))},
+        'heap_sort' : { 'title' : r'Heap Sort ($O(n log(n))$)', 'generator' : lambda data: heap_sort(data)},
+        'quick_sort' : { 'title' : r'Quick Sort ($O(n log(n))$)', 'generator' : lambda data: quick_sort(data, 0, len(data) - 1)},
+    }
 
-def GUI():
-    window = Tk("Visual Sorting")
-    sv = StringVar()
-    start_btn = Button(window, text='START', command=lambda: sort(int(sv.get()), sort_type.get()))
-    start_btn['state'] = 'disabled'
-    size_entry = Entry(window, width=5, textvariable=sv)
-    size_entry.pack(anchor='n')
-    sv.trace('w', lambda name, index, mode, sv=sv, btn=start_btn: callback(sv, btn))
-    start_btn.bind('<Button-1>', )
-    sort_type = StringVar(None, 'selection_sort')
-    Radiobutton(window, text='Selection Sort', variable=sort_type, value='selection_sort').pack(anchor='nw')
-    Radiobutton(window, text='Insertion Sort', variable=sort_type, value='insertion_sort').pack(anchor='nw')
-    Radiobutton(window, text='Bubble Sort', variable=sort_type, value='bubble_sort').pack(anchor='nw')
-    Radiobutton(window, text='Merge Sort', variable=sort_type, value='merge_sort').pack(anchor='nw')
-    Radiobutton(window, text='Heap Sort', variable=sort_type, value='heap_sort').pack(anchor='nw')
-    Radiobutton(window, text='Quick Sort', variable=sort_type, value='quick_sort').pack(anchor='nw')
+class GUI(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.sort_size = IntVar()
+        self.sort_type = StringVar(None, 'selection_sort')
+        self.init_window()
 
-    start_btn.pack(anchor='s')
-    window.mainloop()
+    def init_window(self):
+        def_font = font.nametofont("TkDefaultFont")
+        def_font.configure(size=16)
+        self.master.option_add('*Font', def_font)
+        self.master.resizable(0, 0)
+        self.master.geometry("360x480")
+        self.master.title('Visual Sorting')
+        self.pack(fill=BOTH, expand=0)
+        Label(self, text='Number of elements to be sorted:', width=50).pack(anchor='n')
+        Scale(self, from_=2, to=100, variable=self.sort_size, orient=HORIZONTAL, length=300, width=30 ).pack(anchor='n')
+        Label(self, text='Sorting algorithm:', width=50).pack(anchor='n')
+        radio_group = Frame(self, highlightbackground="black", highlightthickness=1)
+        for v,k in sort_dict.items():
+            Radiobutton(radio_group, text=k.get('title').rsplit('(')[0], variable=self.sort_type, value=v).pack(anchor='w')
+        radio_group.pack(anchor='c')
+        Button(self, text='START', width=15, command=self.start_sorting).pack(side=BOTTOM)
+
+    def start_sorting(self):
+        sort(self.sort_size.get(), self.sort_type.get())
 
 def sort(n = 10, algorithm = 'selection_sort'):
+
     data = [i for i in range(n, 0, -1)]
     shuffle(data)
 
-    sort_dict = {
-        'selection_sort' : { 'title' : r'Selection Sort ($O(n^2)$)', 'generator' : selection_sort(data)},
-        'insertion_sort' : { 'title' : r'Insertion Sort ($O(n^2)$)', 'generator' : insertion_sort(data)},
-        'bubble_sort' : { 'title' : r'Bubble Sort ($O(n^2)$)', 'generator' : bubble_sort(data)},
-        'merge_sort' : { 'title' : r'Merge Sort ($O(n log(n))$)', 'generator' : merge_sort(data, 0, len(data))},
-        'heap_sort' : { 'title' : r'Heap Sort ($O(n log(n))$)', 'generator' : heap_sort(data)},
-        'quick_sort' : { 'title' : r'Quick Sort ($O(n log(n))$)', 'generator' : quick_sort(data, 0, len(data) - 1)},
-    }
-
     fig, ax= plt.subplots()
-
     fig.canvas.set_window_title(sort_dict.get(algorithm).get('title').rsplit('(')[0])
     sort_type = sort_dict.get(algorithm)
 
@@ -67,15 +69,17 @@ def sort(n = 10, algorithm = 'selection_sort'):
         return plt.bar(x_data, y_data, color='m')
 
     FuncAnimation(fig=fig,
-                 func=update,
-                 init_func=init,
-                 frames=sort_type.get('generator'),
-                 blit=True,
-                 interval=1,
-                 repeat=False)
+             func=update,
+             init_func=init,
+             frames=sort_type.get('generator')(data),
+             blit=True,
+             interval=1,
+             repeat=False)
     plt.show()
 
 def main():
-    GUI()
+    root = Tk()
+    app = GUI(master=root)
+    root.mainloop()
 
 main()
